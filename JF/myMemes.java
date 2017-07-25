af723f8d429db66d3cd0b4a3a7fe28f998f065a1
JamieAlizadeh <JamieAlizadeh@gmail.com>
2017-07-24 21:16:25 -0400

    this is updated

---
+++ JF/myMemes.java
@@ -0,0 +1,8 @@
+public class myMemes { 
+	public static void main (String[] args){
+		System.out.println("hello world");
+		for (int i = 0; i < 5; i++){
+			System.out.println("This is updated");
+		}
+	}
+}
