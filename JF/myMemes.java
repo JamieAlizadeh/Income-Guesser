6b7536d58b94eecd1b56d3a85ea07c68e3c20d1f
Jamie Alizadeh <JamieAlizadeh@gmail.com>
2017-08-06 18:29:35 -0400

    Delete myMemes.java

--- JF/myMemes.java
+++
@@ -1,8 +0,0 @@
-public class myMemes { 
-	public static void main (String[] args){
-		System.out.println("hello world");
-		for (int i = 0; i < 5; i++){
-			System.out.println("This is updated");
-		}
-	}
-}
